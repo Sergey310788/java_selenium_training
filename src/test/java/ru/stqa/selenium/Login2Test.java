f4c11bcd85d44196408badae092e83da050c8393
Sergey310788 <tolstikovsergey@gmail.com>
2016-05-18 14:02:13 +0400

    Добавлен тест Login2Test.java Прописан адрес удаленного хаба https://www.browserstack.com

---
+++ src/test/java/ru/stqa/selenium/Login2Test.java
@@ -0,0 +1,18 @@
+package ru.stqa.selenium;
+import org.testng.annotations.Test;
+import org.openqa.selenium.*;
+
+public class Login2Test extends TestNgTestBase {
+
+    @Test
+    public void Login2Test() {
+        driver.get("http://localhost/php4dvd/");
+        driver.findElement(By.id("username")).click();
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("submit")).click();
+    }
+}

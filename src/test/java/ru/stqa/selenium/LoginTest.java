d1e3d5b036cdb805d359d2629984acd28e8103dc
Sergey310788 <tolstikovsergey@gmail.com>
2016-05-25 01:51:36 +0400

    wait for page title

--- src/test/java/ru/stqa/selenium/LoginTest.java
+++ src/test/java/ru/stqa/selenium/LoginTest.java
@@ -1,7 +1,11 @@
 package ru.stqa.selenium;
 
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
+import static org.testng.Assert.*;
 import org.openqa.selenium.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginTest extends TestNgTestBase{
 
@@ -13,5 +17,6 @@ public class LoginTest extends TestNgTestBase{
     driver.findElement(By.name("password")).clear();
     driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.name("submit")).click();
+    assertEquals(driver.findElement(By.xpath("//title")).getText(), " My movie collection - php4dvd v2.0 ");
   }
 }

ae4fa9ef0f66a08bcffea434859f0bd7e0085119
Sergey310788 <tolstikovsergey@gmail.com>
2016-05-26 01:40:35 +0400

    make method testLogin static

--- src/test/java/ru/stqa/selenium/LoginTest.java
+++ src/test/java/ru/stqa/selenium/LoginTest.java
@@ -1,22 +1,18 @@
 package ru.stqa.selenium;
 
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 
-import java.util.concurrent.TimeUnit;
 
 public class LoginTest extends TestNgTestBase{
 
   @Test
-  public void testLogin() throws Exception {
+  public static void testLogin(WebDriver driver) throws Exception {
     driver.get(baseUrl + "/php4dvd/");
     driver.findElement(By.id("username")).clear();
     driver.findElement(By.id("username")).sendKeys("admin");
     driver.findElement(By.name("password")).clear();
     driver.findElement(By.name("password")).sendKeys("admin");
     driver.findElement(By.name("submit")).click();
-    assertEquals(driver.findElement(By.xpath("//title")).getText(), " My movie collection - php4dvd v2.0 ");
   }
 }

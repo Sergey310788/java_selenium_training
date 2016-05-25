e3036764e9242caad5b8ead53821ca0ddde67d19
Sergey310788 <tolstikovsergey@gmail.com>
2016-05-26 01:29:10 +0400

    added AddMovieTest.java and DeleteMovieTest.java

---
+++ src/test/java/ru/stqa/selenium/DeleteMovieTest.java
@@ -0,0 +1,39 @@
+package ru.stqa.selenium;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class DeleteMovieTest extends TestNgTestBase{
+
+  @Test
+    public void testDeleteMovie() throws Exception {
+    //login
+    LoginTest.testLogin(driver);
+
+    //delete movie
+    driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    WebDriverWait wait = new WebDriverWait(driver, 30);
+    List<WebElement> movieBoxesBefore = driver.findElements(By.xpath("//*[@class='movie_box']"));
+    driver.findElement(By.xpath("//*[@class='movie_box']")).click();
+    driver.findElement(By.xpath("//*[@title='Remove']")).click();
+    wait.until(ExpectedConditions.alertIsPresent());
+    Alert alert = driver.switchTo().alert();
+    alert.accept();
+
+    //check movies counter
+    driver.findElement(By.linkText("Home")).click();
+    wait.until(ExpectedConditions. //wait for home page to load
+            presenceOfElementLocated(By.
+                    xpath("//*[@class='searchbox']")));
+    List<WebElement> movieBoxesAfter = driver.findElements(By.xpath("//*[@class='movie_box']"));
+    if (movieBoxesAfter.size() != movieBoxesBefore.size() - 1)
+      throw new Error("Фильм не удален");
+  }
+}

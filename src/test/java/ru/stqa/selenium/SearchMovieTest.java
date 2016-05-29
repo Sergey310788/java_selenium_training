7188657ede7e5e48f87fae185e703d7210b0910a
Sergey310788 <tolstikovsergey@gmail.com>
2016-05-29 21:49:53 +0400

    Добавлены тесты поиска фильмов SearchMovieTest.java

--- src/test/java/ru/stqa/selenium/SearchMovieTest.java
+++ src/test/java/ru/stqa/selenium/SearchMovieTest.java
@@ -2,7 +2,9 @@ package ru.stqa.selenium;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -13,27 +15,31 @@ import java.util.concurrent.TimeUnit;
 public class SearchMovieTest extends TestNgTestBase{
 
   @Test
-    public void testSearchMovie() throws Exception {
+    public void testSearchMoviePositive() throws Exception {
+    //add movie
+    AddMovieTest.addMovie(driver);
+
+    //search movie
+    driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    WebDriverWait wait = new WebDriverWait(driver, 30);
+    driver.findElement(By.xpath("//input[@id='q']")).sendKeys("Начало" + Keys.RETURN);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("//div[contains(text(),'Начало')]")));
+  }
+  @Test
+  public void testSearchMovieNegative() throws Exception {
     //login
     LoginTest.testLogin(driver);
 
     //search movie
     driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     WebDriverWait wait = new WebDriverWait(driver, 30);
-    List<WebElement> movieBoxesBefore = driver.findElements(By.xpath("//*[@class='movie_box']"));
-    driver.findElement(By.xpath("//*[@class='movie_box']")).click();
-    driver.findElement(By.xpath("//*[@title='Remove']")).click();
-    wait.until(ExpectedConditions.alertIsPresent());
-    Alert alert = driver.switchTo().alert();
-    alert.accept();
-
-    //check movies counter
-    driver.findElement(By.linkText("Home")).click();
-    wait.until(ExpectedConditions. //wait for home page to load
-            presenceOfElementLocated(By.
-                    xpath("//*[@class='searchbox']")));
-    List<WebElement> movieBoxesAfter = driver.findElements(By.xpath("//*[@class='movie_box']"));
-    if (movieBoxesAfter.size() != movieBoxesBefore.size() - 1)
-      throw new Error("Фильм не удален");
+    wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("//input[@id='q']")));
+    driver.findElement(By.xpath("//input[@id='q']")).clear();
+    driver.findElement(By.xpath("//input[@id='q']")).sendKeys("Тест" + Keys.RETURN);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("//div[contains(text(),'No movies where found.')]")));
+    driver.findElement(By.xpath("//a[contains(text(),'Home')]")).click();
   }
 }

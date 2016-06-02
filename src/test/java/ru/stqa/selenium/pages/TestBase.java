76d20a5263d9f0172fb501fb388a651a6178b2e6
Sergey Tolstikov <stolstikov@qrream.ru>
2016-06-02 16:56:41 +0300

    Remake ApplicationManager2

--- src/test/java/ru/stqa/selenium/pages/TestBase.java
+++ src/test/java/ru/stqa/selenium/pages/TestBase.java
@@ -4,6 +4,8 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import ru.stqa.selenium.applogic2.ApplicationManager2;
 
+import java.io.IOException;
+
 //import ru.stqa.selenium.applogic0.ApplicationManager0;
 //import ru.stqa.selenium.applogic1.ApplicationManager1;
 
@@ -12,7 +14,7 @@ public class TestBase {
   protected ApplicationManager2 app;
 
 	@BeforeClass
-	public void init() {
+	public void init() throws IOException {
 		app = new ApplicationManager2();
 	}
 	
